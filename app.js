4459b8f1fa108bd1e43002590397e46acdf7b774
PraneethaMuppalla <lakshmimuppalla2453@gmail.com>
2023-12-10 18:33:13 +0530

    Middlewares

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require("express");
-
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  console.log("In middleware");
-  next();
+app.get("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><label for="product">Product</label><input id="product" type="text" name="product" /><br><label for="quantity">Quantity</label><input id="quantity" type="number" name="quantity"><br><button type="submit">Send</button></form>'
+  );
 });
-app.use((req, res) => {
-  console.log("In another middleware");
-  res.send("<h1>Hello from express</h1>");
+
+app.post("/product", (req, res, next) => {
+  console.log("product", req.body.product, "quantity", req.body.quantity);
+  res.redirect("/add-product");
 });
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello from Express Js</h1>");
+});
+
 app.listen(3000);
